9230533bb4ca03500c2aa183fb2e9aa7cc2b75c8
DerGut <jannik.steinmann@gmx.de>
2017-11-11 17:08:51 +0100

    APU in c#

--- ApiRequest.cs
+++ ApiRequest.cs
@@ -39,21 +39,76 @@ class DisplayParameter {
 
 class EfaApi {
 
-	private var client;
-
-	public EfaApi() {
-		client = new RestClient("http://app.vrr.de/msapp/XML_DM_REQUEST");
-	}
-
 	public string getClosestStop() {
-		var request = new RestRequest("", Method.POST);
-		request.AddParameter("name_dm", stopParameter.nameDm);
-		request.AddParameter("type_dm", stopParameter.typeDm);
-		request.AddParameter("coordOutputFormatTail", stopParameter.coordOutputFormatTail);
-		request.AddParameter("outputFormat", stopParameter.outputFormat);
-		request.AddParameter("coordOutputFormat", stopParameter.coordOutputFormat);
+		using (var webClient = new System.Net.WebClient()) {
 
-		client.Execute(request);
+			webClient.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+    		string HtmlResult = wc.UploadString(URI, myParameters);
+
+    		// Define parameters
+    		var request_param = new System.Collections.Specialized.NameValueCollection();
+		    reqparm.Add("name_dm", stopParameter.nameDm);
+		    reqparm.Add("type_dm", stopParameter.typeDm);
+		    reqparm.Add("coordOutputFormatTail", stopParameter.coordOutputFormatTail);
+		    reqparm.Add("outputFormat", stopParameter.outputFormat);
+		    reqparm.Add("coordOutputFormat", stopParameter.coordOutputFormat);
+
+		    // Request
+		    byte[] responsebytes = webClient.UploadValues("http://app.vrr.de/msapp/XML_DM_REQUEST", "POST", request_param);
+		    string responsebody = Encoding.UTF8.GetString(responsebytes);
+
+		    JObject json = JObject.Parse(responsebody);
+
+		    Console.WriteLine(responsebody);
+
+		    // var stop_id = 
+
+		    // Define the second parameters
+    		var request_param_2 = new System.Collections.Specialized.NameValueCollection();
+		    reqparm.Add("name_dm", stop_id);
+		    reqparm.Add("type_dm", "any");
+		    reqparm.Add("coordOutputFormatTail", stopParameter.coordOutputFormatTail);
+		    reqparm.Add("outputFormat", stopParameter.outputFormat);
+		    reqparm.Add("coordOutputFormat", stopParameter.coordOutputFormat);
+		    reqparm.Add("changeSpeed",normal ;
+		    reqparm.Add("nameDm",7.637305:51.951101:WGS84[DD.DDDDD];
+			reqparm.Add("typeDm",coord;
+			reqparm.Add("useRealtime",1
+			reqparm.Add("coordListOutputFormat",STRING;
+			reqparm.Add("canChangeMOT",0;
+			reqparm.Add("sessionID",0;
+			reqparm.Add("itOptionsActive",1;
+		    reqparm.Add("coordOutputFormatTail",5;
+		    reqparm.Add("mode",direct;
+		    reqparm.Add("useAllStops",1;
+		    reqparm.Add("imparedOptionsActive",1;
+		    reqparm.Add("maxTimeLoop",1;
+		    reqparm.Add("includeCompleteStopSeq",1;
+		    reqparm.Add("useProxFootSearch",1;
+		    reqparm.Add("stateless",1;
+		    reqparm.Add("outputFormat",JSON;
+		    reqparm.Add("excludedMeans",checkbox;
+		    reqparm.Add("mergeDep",1;
+		    reqparm.Add("lineRestriction",400;
+		    reqparm.Add("depType",stopEvents;
+		    //reqparm.Add("ptOptionsActive",1;
+		    reqparm.Add("trlTMOTvalue100",15;
+		    reqparm.Add("coordOutputFormat",WGS84[DD.DDDDD];
+		    reqparm.Add("locationServerActive",1;
+		    
+		    //Second request
+		    webClient.UploadValues("http://app.vrr.de/msapp/XML_DM_REQUEST", "POST", request_param_2);
+
+
+
+			var json = webClient.DownloadString("http://app.vrr.de/msapp/XML_DM_REQUEST");
+
+			Newtonsoft.Json.Linq.JObject o = Newtonsoft.Json.Linq.JObject.Parse(json);
+
+			var stops = json["dm"][|"itdOdvAssignedStops"]
+
+			client.Execute(request);
+		}
 	}
 
 }
